90d126fb21f6ad47135d3041f74e301f40cadc62
Maile Lei Pedersen <maybemaile@gmail.com>
2018-10-20 17:59:52 -0700

    starting over after review works up to end of 26

--- js/main.js
+++ js/main.js
@@ -71,8 +71,8 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 /**
  * Initialize leaflet map, called from HTML.
  */
-initMap = () => {
-  if (navigator.onLine) {
+ initMap = () => {
+   if (navigator.onLine) {
      try {
        self.newMap = L.map('map', {
          center: [40.722216, -73.987501],
@@ -83,18 +83,13 @@ initMap = () => {
          mapboxToken: SECRET.mapbox_key,
          maxZoom: 18,
          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
          id: 'mapbox.streets'
        }).addTo(newMap);
      } catch(error) {
        console.log("Map couldn't be initialized", error);
-       // If an error occurred while trying to initialize the map, set map as offline
-       DBHelper.mapOffline();
      }
-   } else {
-     // If app detects we're offline, set map as offline
-     DBHelper.mapOffline();
    }
 
   updateRestaurants();
